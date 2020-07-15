3ad32567bc1d10806f2861b93130a47ccfd40bf2
Winnie Yeung <vionwinnie@gmail.com>
2020-07-14 21:02:52 -0700

    iterative

--- data_structure/binaryTree/iterative_traversal.py
+++ data_structure/binaryTree/iterative_traversal.py
@@ -23,4 +23,35 @@ def preorderTraversal(self,root:TreeNode) -> List[int]:
 
     return all_num
 
+## Post order - use two stacks
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        # 1. Push root to first stack.
+        #2. Loop while first stack is not empty
+            ## 2.1 Pop a node from first stack and push it to second stack
+            ## 2.2 Push left and right children of the popped node to first stack
+        # 3. Print contents of second stack
 
+
+        stack1 = deque()
+        stack2 = deque()
+
+        cur_node = root
+        stack1.append(cur_node)
+
+        while stack1:
+            cur_node = stack1.pop()
+            stack2.append(cur_node)
+
+            if cur_node.left:
+                stack1.append(cur_node.left)
+            if cur_node.right:
+                stack1.append(cur_node.right)
+
+        final_val = []
+        while stack2:
+            last_val = stack2.pop()
+            final_val.append(last_val.val)
+
+        return final_val
+
+## TODO: Iterative In-order traversal
